7c07c2af30161afa2184ff78957e4ce522ce49ad
unknown <fulopmarton99@gmail.com>
2025-07-26 15:28:41 +0300

    Pipeline setup

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,5 @@
+echo "[*] Configuring systemd-resolved for local DNS..."
+
 sudo mkdir -p /etc/systemd/resolved.conf.d
 echo "[Resolve]
 DNS=127.0.0.1
@@ -5,6 +7,5 @@ Domains=~test" >/etc/systemd/resolved.conf.d/test-domains.conf
 
 sudo systemctl restart systemd-resolved
 
-# Install local CA
-sudo apt install mkcert libnss3-tools -y
-mkcert -install
+echo "[*] Installing mkcert root CA..."
+mkcert -install || true
